28fe15d228bf99afbb7eae520ee64b2d309cadee
Amol Chavan <amolchavan0619@gmail.com>
2014-11-05 22:22:39 +0530

    initial commit

--- callJSWithObjectiveC/ViewController.h
+++ callJSWithObjectiveC/ViewController.h
@@ -5,11 +5,12 @@
 //  Created by amol on 05/11/14.
 //
 //
-
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<UIWebViewDelegate>
 
+@property (weak, nonatomic) IBOutlet UILabel *jsString;
+- (IBAction)jsWithobjectiveC:(id)sender;
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
 
 @end
-
--- callJSWithObjectiveC/ViewController.m
+++ callJSWithObjectiveC/ViewController.m
@@ -16,6 +16,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self.webView loadHTMLString:@"<script src=\"demo.js\"></script>"
+                         baseURL:[NSURL fileURLWithPath:[[NSBundle mainBundle] resourcePath]]];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -24,4 +26,13 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)jsWithobjectiveC:(id)sender {
+    NSString *str=@"hi JavaScript";
+    NSString *function = [[NSString alloc] initWithFormat: @"hello('%@')", str];
+    NSString *result = [self.webView stringByEvaluatingJavaScriptFromString:function];
+    self.jsString.text=result;
+    NSLog(result);
+    NSLog(str);
+    
+}
 @end
