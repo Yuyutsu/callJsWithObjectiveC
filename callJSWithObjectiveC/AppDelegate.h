ad85d05e3a7d686c73dc83feae69b90ee963370c
Amol Chavan <amolchavan0619@gmail.com>
2014-11-05 22:10:48 +0530

    Initial Commit

---
+++ callJSWithObjectiveC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  callJSWithObjectiveC
+//
+//  Created by amol on 05/11/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ callJSWithObjectiveC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  callJSWithObjectiveC
+//
+//  Created by amol on 05/11/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
